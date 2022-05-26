f815cfac6a03de0d3fdce81b57cbc86c2a793caa
JohnOK <john.ok.esp@gmail.com>
2022-05-26 03:38:01 +0200

    added edge case to spec file: search term does not exist

--- python/linear_search_spec.py
+++ python/linear_search_spec.py
@@ -1,11 +1,13 @@
 from linear_search import linear_search
 from linear_search import linear_search_global
 
+print("---testing: linear---")
 print(linear_search(3, [1,2,3]) == 2)
 print(linear_search(4, [1,2,3]) == None)
 print(linear_search(13, [1,2,3]) == None)
 
-
+print("\n---testing: global---")
 print(linear_search_global("a", ["b", "a", "n", "a", "n", "a", "s"]) == [1, 3, 5])
 print(linear_search_global("s", ["b", "a", "n", "a", "n", "a", "s"]) == [6])
 print(linear_search_global("n", ["b", "a", "n", "a", "n", "a", "s"]) == [2, 4])
+print(linear_search_global("x", ["b", "a", "n", "a", "n", "a", "s"]) == [])
