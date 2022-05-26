e442beee59c9b8569edb05a3f335954196a4d035
JohnOK <john.ok.esp@gmail.com>
2022-05-26 03:17:32 +0200

    added linear_search function

--- python/linear_search.py
+++ python/linear_search.py
@@ -2,5 +2,16 @@ array_to_search_through = []
 for number in range(1, 1001):
     array_to_search_through.append(number)
 
+
+## linear search
 def linear_search(value_to_find, array_to_search_through):
-    # your code here
+    index = None
+
+    for i in range(len(array_to_search_through)):
+        if (array_to_search_through[i] == value_to_find):
+            index = i
+    
+    return index
+
+
+## global search
