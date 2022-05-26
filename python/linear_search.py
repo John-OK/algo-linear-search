91e94cfedf090b40c9e9875e55411d25404ee19b
JohnOK <john.ok.esp@gmail.com>
2022-05-26 03:46:10 +0200

    added searching empty lists; added test for searcching empty lists

--- python/linear_search.py
+++ python/linear_search.py
@@ -18,6 +18,9 @@ def linear_search(value_to_find, array_to_search_through):
 def linear_search_global(value_to_find, array_to_search_through):
     indices_of_values = []
 
+    if not array_to_search_through:
+        return []
+
     for i in range(len(array_to_search_through)):
         if (array_to_search_through[i] == value_to_find):
             indices_of_values.append(i)
