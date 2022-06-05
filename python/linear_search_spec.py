70994b705396d869a07035b5b3b6ac21600f8544
JohnOK <john.ok.esp@gmail.com>
2022-06-05 04:33:54 +0200

    Added another test to spec file.

--- python/linear_search_spec.py
+++ python/linear_search_spec.py
@@ -2,9 +2,9 @@ from linear_search import linear_search
 from linear_search import linear_search_global
 
 print("---testing: linear---")
-print(linear_search(3, [1,2,3]) == 2)
-print(linear_search(4, [1,2,3]) == None)
-print(linear_search(13, [1,2,3]) == None)
+print(linear_search(3, [1, 2, 3]) == 2)
+print(linear_search(4, [1, 2, 3]) == None)
+print(linear_search(13, [1, 2, 3]) == None)
 print(linear_search(13, []) == None)
 
 print("\n---testing: global---")
